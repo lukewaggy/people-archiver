ee0d2a363dcf085e855e517ef889aad8a04e9041
Kyle Hinkel <kylehinkel@gmail.com>
2016-10-25 18:44:42 -0400

    Import objects

--- archivercli.py
+++ archivercli.py
@@ -1,3 +1,7 @@
 from listgetter import ListGetter
+from newapplicant import NewApplicant
+from viewapplicant import ViewApplicant
+from removeapplicant import RemoveApplicant
+
 class ArchiverCLI:
     print "Hi, this is the people archiver, please type 'new', 'view', or 'remove'."
