39ed2664766d4eb3c7f09375132291e0c08118c8
Kyle Hinkel <kylehinkel@gmail.com>
2016-10-25 19:35:53 -0400

    Progress

--- archivercli.py
+++ archivercli.py
@@ -1,17 +1,21 @@
 from listgetter import ListGetter
+from listhandler import ListHandler
 from newapplicant import NewApplicant
 from viewapplicant import ViewApplicant
 from removeapplicant import RemoveApplicant
 
 def start():
+    
     commands = [NewApplicant(), ViewApplicant(), RemoveApplicant()]
+    db = ListGetter()
+    db.get_list("database.txt")
+    
     print("Welcome to the people archiver.")
     while(True):
         userInput = input("Please type 'new', 'view', or 'remove': ")
         for cmd in commands:
             if cmd.name == userInput:
-                #cmd.execute()
-                print("grats")
+                cmd.execute()
         
 
 start()
--- listgetter.py
+++ listgetter.py
@@ -1,2 +1,8 @@
 class ListGetter:
-    pass
+    
+    def __init__(self):
+        self.database = []
+
+    def get_list(self, fileName):
+        with open(fileName, 'r') as f:
+            print(f.read())
