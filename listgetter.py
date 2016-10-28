b8196802a16da74260ae33db54141e056d5905a1
Kyle Hinkel <kylehinkel@gmail.com>
2016-10-27 21:35:51 -0400

    Fix ding dongs

--- listgetter.py
+++ listgetter.py
@@ -6,10 +6,15 @@ class ListGetter:
         self.database = []
 
     def get_list(self, fileName):
+
+        # check to see if our database file is there or not
         if os.path.exists(fileName):   
             print("Previous database found.")
         else:
+            # else create a new text file named fileName
             open(fileName, 'w')
 
-        with open(fileName, 'r') as f:
-            print(f.read())
+        populate_list()
+
+    def populate_list(self):
+        pass
