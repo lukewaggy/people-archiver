d7a45b9a54a10ea9ad39625fb7e49d219a07f5db
Kyle Hinkel <kylehinkel@gmail.com>
2016-10-27 21:29:44 -0400

    Add creation of database file

--- listgetter.py
+++ listgetter.py
@@ -1,8 +1,15 @@
+import os
+
 class ListGetter:
     
     def __init__(self):
         self.database = []
 
     def get_list(self, fileName):
+        if os.path.exists(fileName):   
+            print("Previous database found.")
+        else:
+            open(fileName, 'w')
+
         with open(fileName, 'r') as f:
             print(f.read())
